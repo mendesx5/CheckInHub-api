8c36eefbae3e88aef3bd84d651c8b3fee3ffa44d
Gabriel Mendes <bielmendes2208@gmail.com>
2026-07-24 14:11:05 -0300

    Starter a new Java project

---
+++ src/main/java/com/mendes/check_in_hub/CheckInHubApplication.java
@@ -0,0 +1,13 @@
+package com.mendes.check_in_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckInHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mendes/check_in_hub/CheckInHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendes.check_in_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
