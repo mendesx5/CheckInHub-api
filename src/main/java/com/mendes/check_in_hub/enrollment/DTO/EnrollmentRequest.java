81fb6b79367220b2cc72a1176712338483150365
Gabriel Mendes <bielmendes2208@gmail.com>
2026-08-06 21:25:24 -0300

    Mudança de tipo em EnrollmentRequest

--- src/main/java/com/mendes/check_in_hub/enrollment/DTO/EnrollmentRequest.java
+++ src/main/java/com/mendes/check_in_hub/enrollment/DTO/EnrollmentRequest.java
@@ -1,10 +1,8 @@
 package com.mendes.check_in_hub.enrollment.DTO;
 
-import com.mendes.check_in_hub.event.Event;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record EnrollmentRequest(
         @NotNull(message = "The event ID is mandatory")
-        Event eventId
+        Long eventId
 ) {}
