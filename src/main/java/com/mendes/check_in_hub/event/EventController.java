f1551fb27e97ee3d0911855d1b16c4bdb4b3c698
Gabriel Mendes <bielmendes2208@gmail.com>
2026-08-06 22:23:20 -0300

    Creation of the method create in EventController

--- src/main/java/com/mendes/check_in_hub/event/EventController.java
+++ src/main/java/com/mendes/check_in_hub/event/EventController.java
@@ -1,14 +1,39 @@
 package com.mendes.check_in_hub.event;
 
+import com.mendes.check_in_hub.event.DTO.EventRequest;
+import com.mendes.check_in_hub.event.DTO.EventResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/events")
 @RequiredArgsConstructor
 public class EventController {
 
+    private  final EventService eventService;
 
+    @PostMapping
+    public ResponseEntity<EventResponse> createEvent (
+            @RequestBody @Valid EventRequest request,
+            @RequestBody Long organizerId
+    ) {
+        EventResponse eventResponse = eventService.createEvent(request, organizerId);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(eventResponse.id())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 
 }
