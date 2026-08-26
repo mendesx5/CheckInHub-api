ee81c1abb20d490a322b9e3f84b5357b516264c0
Gabriel Mendes <bielmendes2208@gmail.com>
2026-08-25 22:18:22 -0300

    Deactivating the test

--- src/test/java/com/mendes/check_in_hub/CheckInHubApplicationTests.java
+++ src/test/java/com/mendes/check_in_hub/CheckInHubApplicationTests.java
@@ -1,8 +1,10 @@
 package com.mendes.check_in_hub;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Integration tests will be configured later")
 @SpringBootTest
 class CheckInHubApplicationTests {
 
