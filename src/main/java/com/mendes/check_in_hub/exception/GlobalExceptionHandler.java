527a5206b2179af7d273a767a18e6de2327c68d5
mendesx5 <bielmendes2208@gmail.com>
2026-09-19 21:32:15 -0300

    Updating GlobalExceptionHandler

--- src/main/java/com/mendes/check_in_hub/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mendes/check_in_hub/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.mendes.check_in_hub.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,6 +12,8 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     private ResponseEntity<ApiError> buildResponse(
             HttpStatus status,
@@ -84,6 +88,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handleGenericException(Exception e) {
+        log.error("Unhandled exception", e);
+
         return buildResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR,
                 "An unexpected error occurred"
