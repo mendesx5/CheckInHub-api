e0d3abc41585a758f8025d78563ea94436a2386f
mendesx5 <bielmendes2208@gmail.com>
2026-09-19 21:05:05 -0300

    Updating SecurityConfig

--- src/main/java/com/mendes/check_in_hub/security/SecurityConfig.java
+++ src/main/java/com/mendes/check_in_hub/security/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://check-in-hub-web.vercel.app"));
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
         );
